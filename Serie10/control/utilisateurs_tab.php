9c98d4fb990a59eda3688cc5bead98059fdc590c
robertodeguglielmo <roberto.deguglielmo@gmail.com>
2018-06-08 20:35:53 +0200

    Création d'un nouvel utilisateur

--- Serie10/control/utilisateurs_tab.php
+++ Serie10/control/utilisateurs_tab.php
@@ -20,9 +20,12 @@ if(Control_util::isAjax()){
 	$monFormulaire->addText('Prénom :','PRENOM','PRENOM',$VarPrenom,false,'Entrez ici le prénom recherché');
 	/*$monFormulaire->addSubmit('VALIDER','Valider');*/
 	echo $monFormulaire->getForm();
-
 }
-
+if(Control_util::isAjax()){
+	$monFormulaire2 = new Form('NewUtilisateur_tab','post','utilisateurs_fic.php');
+	$monFormulaire2->addSubmit('VALIDER','Ajouter un utilisateur');
+	echo $monFormulaire2->getForm();
+}
 $Utilisateurs=Model::load("utilisateurs");
 $Utilisateurs->read(null,$where);
 require '../vue/utilisateurs_tab.php' ;
--- Serie10/vue/js/utilisateurs.js
+++ Serie10/vue/js/utilisateurs.js
@@ -30,41 +30,56 @@ var  clicKeyUp = function(){
 var  clicModif = function(){
 	var elem = $( this );
 	$destination='utilisateurs_fic.php';
-	$.post( $destination, { UTILISATEUR : $(this).parent().attr('id') } )
+	$.post( $destination, { UTILISATEUR : $(this).parent().attr('id') })
 	.done(function( data ) {
-		$("#myModal").remove();
-		$VarTmp = '<div class="modal fade" id="myModal" role="dialog">'+
-		'<div class="modal-dialog modal-sm">'+
-		'<div class="modal-content">'+
-		'<div class="modal-header">'+
-		'<button type="button" class="close" data-dismiss="modal">&times;</button>'+
-		'<h4 class="modal-title">Modal Header</h4>'+
-		'</div>'+
-		'<div class="modal-body">'+
-		'  <p></p>'+
-		'</div>'+
-		'</div>'+
-		'</div>'+
-		'</div>';
-
-		$('body').append($VarTmp);
-		$('.modal-body').html(data);
-		$("#myModal").modal();
-		$("#myModal form").submit(function( event ) {
-			event.preventDefault();
-			$.post('utilisateurs_fic.php',$(this).serialize()).done(function(data) {});
-			$("#myModal").modal('toggle');
-			$("form #PRENOM").trigger('keyup');
-
-		});
+		displayModal(data);
 	});
 };
 
+var  clicNew = function(event){
+	event.preventDefault();
+	var elem = $( this );
+	$destination='utilisateurs_fic.php';
+	$.post( $destination)
+	.done(function( data ) {
+		displayModal(data);
+	});
+};
+
+var displayModal= function(data){
+	$("#myModal").remove();
+	$VarTmp = '<div class="modal fade" id="myModal" role="dialog">'+
+	'<div class="modal-dialog modal-sm">'+
+	'<div class="modal-content">'+
+	'<div class="modal-header">'+
+	'<button type="button" class="close" data-dismiss="modal">&times;</button>'+
+	'<h4 class="modal-title">Modal Header</h4>'+
+	'</div>'+
+	'<div class="modal-body">'+
+	'  <p></p>'+
+	'</div>'+
+	'</div>'+
+	'</div>'+
+	'</div>';
+
+	$('body').append($VarTmp);
+	$('.modal-body').html(data);
+	$("#myModal").modal();
+	$("#myModal form").submit(function( event ) {
+		event.preventDefault();
+		$.post('utilisateurs_fic.php',$(this).serialize()).done(function(data) {});
+		$("#myModal").modal('toggle');
+		$("form #PRENOM").trigger('keyup');
+
+	});
+}
 
 $(function(){
 	$(document).on('click','#utilisateurs #actif',clicActive);
 	$(document).on('click','#utilisateurs #modifier',clicModif);
-	$(document).on('change','form #PRENOM',clicKeyUp);
-	$(document).on('keyup','form #PRENOM',clicKeyUp);
+	$(document).on('change','form[name="FormUtilisateur_tab"] #PRENOM',clicKeyUp);
+	$(document).on('keyup','form[name="FormUtilisateur_tab"] #PRENOM',clicKeyUp);
+	$('form[name="NewUtilisateur_tab"]').submit(function( event ) {clicNew(event)});
+	
 });
 
